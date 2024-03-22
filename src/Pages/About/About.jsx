21dca041d2ebf6dfa4fcad3adf9907a8aec44b68
akashkottil <kottilakash@gmail.com>
2024-03-22 12:46:51 +0530

    config

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Navbar from '../../Components/Navbar/Navbar'
+// import Navbar from '../../Components/Navbar/Navbar'
 import AboutUs from '../../Components/AboutUs/AboutUs'
 
 const About = () => {
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: '/Fastreboot-Landing-Page/', // Set the base URL for GitHub Pages
+  plugins: [react()]
 })
