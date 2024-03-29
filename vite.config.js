ed38915d55661420dc57845d389838bfa0db388f
Imad Ibrahim <imadibrahim164@gmail.com>
2024-03-29 16:20:18 +0530

    second commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   base: '/', // Set the base URL for GitHub Pages
+//   plugins: [react()]
+// })
+
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
-  base: '/', // Set the base URL for GitHub Pages
-  plugins: [react()]
-})
+  base: '/fastreboot-website/', // Set the base URL for GitHub Pages
+  plugins: [react()],
+});
